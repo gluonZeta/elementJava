77e129104aa3ab1cccdd4460f928ee03e1c1a736
gluonZeta <andrainat@gmail.com>
2025-03-30 09:07:30 +0100

    Add function to KO jacoco

---
+++ elementJava_service/src/main/java/gluon/projects/JacocoCoverageAnalyseServiceLayer.java
@@ -0,0 +1,13 @@
+package gluon.projects;
+
+public class JacocoCoverageAnalyseServiceLayer {
+
+    public float soustraction(float a, float b) {
+        return a-b;
+    }
+
+    public float multiplication(float a, float b) {
+        return a*b;
+    }
+
+}

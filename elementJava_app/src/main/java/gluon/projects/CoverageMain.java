72f516dbd4c3e2d295b894a624c3b140c58ff9e1
gluonZeta <andrainat@gmail.com>
2025-03-30 05:57:21 +0100

    psvm dans la fonction main

---
+++ elementJava_app/src/main/java/gluon/projects/CoverageMain.java
@@ -0,0 +1,13 @@
+package gluon.projects;
+
+public class CoverageMain {
+
+    public int additionInt(int a, int b) {
+        return a+b;
+    }
+
+    public int multiplicationInt(int a, int b) {
+        return a*b;
+    }
+
+}
--- elementJava_app/src/main/java/gluon/projects/Main.java
+++ elementJava_app/src/main/java/gluon/projects/Main.java
@@ -2,12 +2,8 @@ package gluon.projects;
 
 public class Main {
 
-    public int additionInt(int a, int b) {
-        return a+b;
-    }
-
-    public int multiplicationInt(int a, int b) {
-        return a*b;
+    public static void main(String[] args) {
+        System.out.println("Trigonometrie");
     }
 
 }

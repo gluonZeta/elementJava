9f240f21c701852dd8abf0a4efb7fb56f651cff8
gluonZeta <andrainat@gmail.com>
2025-03-30 09:57:29 +0100

    Modification to add test coverage

---
+++ elementJava_service/src/test/java/gluon/projects/JacocoCoverageAnalyseServiceLayerTest.java
@@ -0,0 +1,25 @@
+package gluon.projects;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class JacocoCoverageAnalyseServiceLayerTest {
+
+    private JacocoCoverageAnalyseServiceLayer jacocoCoverageAnalyseServiceLayer;
+
+    @BeforeEach
+    void setUp() {
+        this.jacocoCoverageAnalyseServiceLayer = new JacocoCoverageAnalyseServiceLayer();
+    }
+
+    @Test
+    void soustraction() {
+        assertEquals(4.3F, this.jacocoCoverageAnalyseServiceLayer.soustraction(5.4F,1.1F));
+    }
+
+    @Test
+    void multiplication() {
+        assertEquals(24F, this.jacocoCoverageAnalyseServiceLayer.multiplication(8F,3F));
+    }
+}

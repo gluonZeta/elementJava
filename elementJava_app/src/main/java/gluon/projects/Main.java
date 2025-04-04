e6ca5f3508004a62e4a8b7b3279fb36cf6b2252b
gluonZeta <andrainat@gmail.com>
2025-04-04 06:09:59 +0100

    Clean and initialisation de log4j and the project

--- elementJava_app/src/main/java/gluon/projects/Main.java
+++ elementJava_app/src/main/java/gluon/projects/Main.java
@@ -1,9 +1,20 @@
 package gluon.projects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Figaro Delta Gero");
+        logger.trace("Ceci est un log TRACE");
+        logger.debug("Ceci est un log DEBUG");
+        logger.info("Ceci est un log INFO");
+        logger.warn("Ceci est un log WARN");
+        logger.error("Ceci est un log ERROR");
+        logger.fatal("Ceci est un log FATAL");
+
     }
 
 }
--- elementJava_service/src/main/java/gluon/projects/JacocoCoverageAnalyseServiceLayer.java
+++ elementJava_service/src/main/java/gluon/projects/JacocoCoverageAnalyseServiceLayer.java
@@ -1,8 +1,14 @@
 package gluon.projects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class JacocoCoverageAnalyseServiceLayer {
 
+    private Logger logger = LogManager.getLogger(JacocoCoverageAnalyseServiceLayer.class);
+
     public float soustraction(float a, float b) {
+        logger.info("Calcul soustraction SERVICE LAYER");
         return a-b;
     }
 

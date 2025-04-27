1efb13f959303e507b33b30a30f8c34f0b8aea73
gluonZeta <andrainat@gmail.com>
2025-04-27 12:15:22 +0100

    Test to reduce test coverage but not

--- elementJava_service/src/test/java/gluon/projects/UtilsTest.java
+++ elementJava_service/src/test/java/gluon/projects/UtilsTest.java
@@ -1,14 +1,17 @@
 package gluon.projects;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class UtilsTest {
 
     @Test
     void getPropertiesByFileName() {
         Properties properties = Utils.getPropertiesByFileName("application.properties");
-        Assertions.assertNotNull(properties);
+        assertNotNull(properties);
     }
+
 }

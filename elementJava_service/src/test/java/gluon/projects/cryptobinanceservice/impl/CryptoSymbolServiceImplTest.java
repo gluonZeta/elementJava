eb4ea40cbb86bd34ee9e379410ca9e99906e2580
gluonZeta <andrainat@gmail.com>
2025-04-27 08:37:19 +0100

    Add test for coverage

---
+++ elementJava_service/src/test/java/gluon/projects/cryptobinanceservice/impl/CryptoSymbolServiceImplTest.java
@@ -0,0 +1,28 @@
+package gluon.projects.cryptobinanceservice.impl;
+
+import gluon.projects.cryptobinanceservice.CryptoSymbolService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CryptoSymbolServiceImplTest {
+
+    CryptoSymbolService cryptoSymbolService;
+
+    @BeforeEach
+    void setUp() {
+        this.cryptoSymbolService = new CryptoSymbolServiceImpl();
+    }
+
+    @Test
+    void getNewListSymbol() {
+        Assertions.assertNotNull(this.cryptoSymbolService.getNewListSymbol());
+    }
+
+    @Test
+    void getExistListSymbol() {
+        Assertions.assertNotNull(this.cryptoSymbolService.getExistListSymbol());
+    }
+}

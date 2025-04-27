0394cb771d4f0da06378a3e323643125e9c6b85c
gluonZeta <andrainat@gmail.com>
2025-04-27 07:59:58 +0100

    Dynamic add logback level

--- elementJava_app/src/main/java/gluon/projects/Main.java
+++ elementJava_app/src/main/java/gluon/projects/Main.java
@@ -1,8 +1,18 @@
 package gluon.projects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.trace("Ceci est un log TRACE");
+        logger.debug("Ceci est un log DEBUG");
+        logger.info("Ceci est un log INFO");
+        logger.warn("Ceci est un log WARN");
+        logger.error("Ceci est un log ERROR");
     }
 
 }

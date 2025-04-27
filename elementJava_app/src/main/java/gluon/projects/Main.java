be4e6f5b67f45ef654cbcbd1e80a24713aedd43b
gluonZeta <andrainat@gmail.com>
2025-04-27 08:19:18 +0100

    Reinitialisation for Main coverage

--- elementJava_app/src/main/java/gluon/projects/Main.java
+++ elementJava_app/src/main/java/gluon/projects/Main.java
@@ -5,14 +5,17 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    //private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        /*
         logger.trace("Ceci est un log TRACE");
         logger.debug("Ceci est un log DEBUG");
         logger.info("Ceci est un log INFO");
         logger.warn("Ceci est un log WARN");
         logger.error("Ceci est un log ERROR");
+
+         */
     }
 
 }

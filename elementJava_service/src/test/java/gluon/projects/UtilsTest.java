009719d0437d5e0746b9a6808336bae4896a5f94
gluonZeta <andrainat@gmail.com>
2025-04-28 20:09:40 +0100

    Add Unit test

--- elementJava_service/src/test/java/gluon/projects/UtilsTest.java
+++ elementJava_service/src/test/java/gluon/projects/UtilsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class UtilsTest {
@@ -12,6 +13,7 @@ class UtilsTest {
     void getPropertiesByFileName() {
         Properties properties = Utils.getPropertiesByFileName("application.properties");
         assertNotNull(properties);
+        assertEquals("/home/andraina/Documents/java_projects", properties.getProperty("listsymbolfolder"));
     }
 
 }

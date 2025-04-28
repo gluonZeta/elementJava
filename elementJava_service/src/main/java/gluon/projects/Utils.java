e80cfbcb47ca8582ac3239c8c015342ceb8659a1
gluonZeta <andrainat@gmail.com>
2025-04-28 20:24:39 +0100

    Add send request to get DATA

--- elementJava_service/src/main/java/gluon/projects/Utils.java
+++ elementJava_service/src/main/java/gluon/projects/Utils.java
@@ -4,12 +4,34 @@ import gluon.projects.exceptions.ElementProjectException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Properties;
 
 public class Utils {
 
     private Utils() {
         throw new IllegalStateException("Utility class");
+    }
+
+    public static String sendRequestWithCompleteUrl(String completeUrl) {
+        HttpRequest httpRequest = HttpRequest
+                .newBuilder()
+                .uri(URI.create(completeUrl))
+                .build();
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> httpResponse = null;
+
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new ElementProjectException(e);
+        } catch (InterruptedException e) {
+            throw new ElementProjectException(e);
+        }
+        return httpResponse.body();
     }
 
     public static Properties getPropertiesByFileName(String fileName) {
--- elementJava_service/src/test/java/gluon/projects/UtilsTest.java
+++ elementJava_service/src/test/java/gluon/projects/UtilsTest.java
@@ -1,5 +1,6 @@
 package gluon.projects;
 
+import jdk.jshell.execution.Util;
 import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
@@ -10,10 +11,19 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class UtilsTest {
 
     @Test
+    void sendRequestWithCompleteUrl() {
+        Properties properties = Utils.getPropertiesByFileName("application.properties");
+        StringBuilder url = new StringBuilder(properties.getProperty("apibinanceurl"));
+        url.append("/ping");
+        String response = Utils.sendRequestWithCompleteUrl(url.toString());
+        assertEquals("{}", response);
+    }
+
+    @Test
     void getPropertiesByFileName() {
         Properties properties = Utils.getPropertiesByFileName("application.properties");
         assertNotNull(properties);
-        assertEquals("/home/andraina/Documents/java_projects", properties.getProperty("listsymbolfolder"));
+        assertEquals("/home/andraina/Documents/java_projects/LIST_SYMBOL.txt", properties.getProperty("listsymbolfile"));
     }
 
 }

8ee7090df8660d02955e45f4b4396695543aadbe
gluonZeta <andrainat@gmail.com>
2025-05-01 10:41:18 +0100

    Revert "Without margin filter"
    This reverts commit 80edf2e90cbc560042e7960ee20e41d155fd8715.

--- elementJava_service/src/main/java/gluon/projects/cryptobinanceservice/impl/CryptoSymbolServiceImpl.java
+++ elementJava_service/src/main/java/gluon/projects/cryptobinanceservice/impl/CryptoSymbolServiceImpl.java
@@ -112,6 +112,7 @@ public class CryptoSymbolServiceImpl implements CryptoSymbolService {
             symbol = (String) symbolInfo.get("symbol");
             isMarginTradingAllowed = (boolean) symbolInfo.get("isMarginTradingAllowed");
             if(!symbolExcludes.contains(symbol)
+                    && isMarginTradingAllowed
                     && filterStringSymbol(symbol)) {
                 symbolList.add(symbol);
             }
